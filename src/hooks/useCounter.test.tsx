d3bf652cced9e6074c11e3cd0fcc3a08d44e7fb9
Yoshihiro Sasago <sasayoshi.0605@gmail.com>
2024-01-14 14:25:05 +0900

    フックのテスト

---
+++ src/hooks/useCounter.test.tsx
@@ -0,0 +1,15 @@
+import { act, renderHook } from "@testing-library/react";
+import useCounter from "./useCounter";
+
+describe("useCounter", () => {
+  it("increments the count", () => {
+    const { result } = renderHook(() => useCounter(1));
+    expect(result.current.count).toBe(1);
+
+    act(() => result.current.increment());
+    expect(result.current.count).toBe(2);
+
+    act(() => result.current.decrement());
+    expect(result.current.count).toBe(1);
+  });
+});
